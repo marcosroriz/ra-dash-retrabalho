f3f29a772cb1a67ee379e5d10449b52a1d4f2884
Marcos Roriz <marcosrorizinf@gmail.com>
2025-10-30 11:34:55 -0300

    Workaround para problema de conexão

--- src/db.py
+++ src/db.py
@@ -6,6 +6,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from threading import Lock
 
+# SSH tunelling
+from sshtunnel import SSHTunnelForwarder
+
 
 class PostgresSingleton:
     """
@@ -33,6 +36,12 @@ class PostgresSingleton:
             # Avoid re-initialization
             return
 
+        ssh_host = os.getenv("SSH_HOST")
+        ssh_port = int(os.getenv("SSH_PORT"))
+        ssh_user = os.getenv("SSH_USER")
+        ssh_pass = os.getenv("SSH_PASS")
+        local_bind_port = int(os.getenv("LOCAL_BIND_PORT", 5510))
+
         db_host = os.getenv("DB_HOST")
         db_port = os.getenv("DB_PORT")
         db_user = os.getenv("DB_USER")
@@ -40,7 +49,26 @@ class PostgresSingleton:
         db_name = os.getenv("DB_NAME")
         debug_mode = bool(os.getenv("APP_DEBUG", True))
 
+        # Instancia o túnel SSH
+        self._tunnel = SSHTunnelForwarder(
+            (ssh_host, ssh_port),
+            ssh_username=ssh_user,
+            ssh_password=ssh_pass,
+            remote_bind_address=(db_host, int(db_port)),
+            local_bind_address=("127.0.0.1", local_bind_port),  # local port
+        )
+
+        self._tunnel.start()
+        print(f"✅ SSH tunnel active: 127.0.0.1:{self._tunnel.local_bind_port} → {db_host}:{db_port}")
+
+
+        # Cria conexão via URL / SSH
         db_url = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+        db_url = (
+            f"postgresql://{db_user}:{db_pass}"
+            f"@127.0.0.1:{self._tunnel.local_bind_port}/{db_name}"
+        )
+
         self._engine = create_engine(
             db_url,
             pool_size=10,  # Número de conexões na pool
@@ -68,3 +96,12 @@ class PostgresSingleton:
         Retorna a SQLAlchemy session
         """
         return self._Session()
+
+    def close(self):
+        """Fecha o túnel SSH e o engine"""
+        if hasattr(self, "_engine"):
+            self._engine.dispose()
+        if hasattr(self, "_tunnel") and self._tunnel.is_active:
+            self._tunnel.stop()
+        self._initialized = False
+        print("🔒 Connection and tunnel closed.")
